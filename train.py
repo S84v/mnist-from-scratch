fd6a421224f8d11f7e6dea6f68c755bd12fb7a81
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-05-25 20:08:34 +0530

    Added color formatting for terminal output

--- train.py
+++ train.py
@@ -1,14 +1,24 @@
 from model import NeuralNetwork
 from utils import load_images, load_labels, accuracy
 from loss import cross_entropy_loss
+import os
+
+# Color formatting for terminal output
+os.system("")
+
+YELLOW = "\033[93m"
+GREEN = "\033[92m"
+WHITE = "\033[97m"
+RESET = "\033[0m"
 
 X_train = load_images(r"data\train-images.idx3-ubyte")
 y_train = load_labels(r"data\train-labels.idx1-ubyte")
 
 model = NeuralNetwork()
 
-epochs = 20
-learning_rate = 0.1
+epochs = int(input("Epochs: "))
+learning_rate = float(input("Learning rate: "))
+print()
 
 for epoch in range(epochs):
     y_pred = model.forward(X_train)
@@ -20,4 +30,6 @@ for epoch in range(epochs):
 
     model.update_parameters(dW1, db1, dW2, db2, learning_rate)
 
-    print(f"Epoch: {epoch+1} | Loss: {loss:.4f} | Accuracy: {accuracy_score:.4f}")
+    print(
+        f"{WHITE}Epoch: {epoch+1}{RESET} |{YELLOW} Loss: {loss:.4f}{RESET} |{GREEN} Accuracy: {accuracy_score:.4f}{RESET}"
+    )

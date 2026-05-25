3675a135c828b5058e88419ae41b2baad5625547
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-05-25 19:09:37 +0530

    Added backprop

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import numpy as np
-from activations import relu, softmax
+from activations import relu, relu_derivative, softmax
 
 
 class NeuralNetwork:
@@ -13,10 +13,33 @@ class NeuralNetwork:
 
     def forward(self, X):
 
-        self.Z1 = np.add(np.dot(X, self.W1), self.b1)
-        self.A1 = relu(self.Z1)
+        self.Z1 = np.add(np.dot(X, self.W1), self.b1)  # Linear calculation
+        self.A1 = relu(
+            self.Z1
+        )  # Non linear activation function => non linear value calculation
 
-        self.Z2 = np.add(np.dot(self.A1, self.W2), self.b2)
-        self.A2 = softmax(self.Z2)
+        self.Z2 = np.add(np.dot(self.A1, self.W2), self.b2)  # Linear calculation
+        self.A2 = softmax(self.Z2)  # Non linear calculation
 
         return self.A2
+
+    def backward(self, X, y):
+
+        m = X.shape[0]
+
+        #  output layer gradiesnt
+        dZ2 = self.A2.copy()
+        dZ2[np.arange(m), y] -= 1
+        dZ2 /= m
+
+        dW2 = np.dot(self.A2.T, dZ2)
+        db2 = np.sum(dZ2, axis=0, keepdims=True)
+
+        #  hidden layer(s) gradient(s)
+        dA1 = np.dot(dZ2, self.W2.T)
+        dZ1 = dA1 * relu_derivative(self.Z1)
+
+        dW1 = np.dot(X.T, dZ1)
+        db1 = np.sum(dZ1, axis=0, keepdims=True)
+
+        return dW1, db1, dW2, db2
